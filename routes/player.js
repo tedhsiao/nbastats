0d2ec9a4f2a697bf56e488647e418c215110d637
Ted Hsiao <abc810221@gmail.com>
2017-07-04 21:32:32 -0700

    Add season param for GET /player api

--- routes/player.js
+++ routes/player.js
@@ -3,19 +3,25 @@ const btoa = require("btoa");
 const msf = require("../config/publicKeys");
 const request = require("request");
 
-playerRouter.get("/:player", function(req, res) {
+//@season in the format of year-playoff or year-year-regular
+//@player in the foramt of firstName-lastName or lastName
+playerRouter.get("/:season/:player", function(req, res) {
   let params = req.params;
   let _player = params.player.trim().replace(/\s+/g, "-");
+  let season = params.season;
+  let _url =
+    "https://www.mysportsfeeds.com/api/feed/pull/nba/" +
+    season +
+    "/cumulative_player_stats.json";
   let options = {
-    url:
-      "https://www.mysportsfeeds.com/api/feed/pull/nba/2017-playoff/cumulative_player_stats.json",
+    url: _url,
     headers: {
       Authorization:
         "Basic " +
         btoa(msf.mysportsfeedUsername + ":" + msf.mysportsfeedPassword)
     },
     qs: {
-      playerstats: "2PA,2PM,3PA,3PM,FTA,FTM",
+      playerstats: "MIN/G,+/-/G,BS/G,STL/G,TOV/G,PTS/G,AST/G,REB/G,FT%,FG%,3P%",
       player: _player
     }
   };
