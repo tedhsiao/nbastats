bbf98c64bcf27bacd3fc1b37e795db62b5a7c439
Ted Hsiao <abc810221@gmail.com>
2017-07-22 22:39:48 -0700

    Allow user to select season to search player stats

--- webapp/src/app/players/players.component.ts
+++ webapp/src/app/players/players.component.ts
@@ -11,16 +11,33 @@ export class PlayersComponent implements OnInit {
   private player: object;
   private stats: object;
   private team: object;
-  constructor(private http: Http) {}
+  private noPlayerFoundFlag: boolean = false;
+  private seasons: Array<string> = [];
+  private selectedSeason: string;
+
+  constructor(private http: Http) {
+    for (let i = 2017; i > 2000; i--) {
+      this.seasons.push(`${i - 1}-${i}-regular`);
+      this.seasons.push(`${i}-playoff`);
+    }
+    this.selectedSeason = this.seasons[0];
+  }
 
   ngOnInit() {}
 
   searchPlayer(event: any) {
     this.http
-      .get(`http://localhost:8080/player/2016-playoff/${this.playerName}`)
+      .get(
+        `http://localhost:8080/player/${this.selectedSeason}/${this.playerName}`
+      )
       .map(res => res.json())
       .subscribe(stats => {
-        console.log(stats);
+        if (!stats.cumulativeplayerstats.playerstatsentry) {
+          this.player = null;
+          this.noPlayerFoundFlag = true;
+          return;
+        }
+        this.noPlayerFoundFlag = false;
         this.player = stats.cumulativeplayerstats.playerstatsentry[0].player;
         this.stats = [stats.cumulativeplayerstats.playerstatsentry[0].stats];
         this.team = stats.cumulativeplayerstats.playerstatsentry[0].team;
